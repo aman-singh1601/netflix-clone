0e73b5068695934762336ef7aad2d5344b170dcc
Aman <aman16singh2001@gmail.com>
2023-06-11 15:50:52 +0530

    built the nav bar and added the css

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+
+
 
 function Nav() {
+    const [show,setShow]=useState(false)
+   
+    useEffect(()=>{
+        const handleScroll= ()=>{
+            if(window.scrollY>150){
+                setShow(true);
+            }else setShow(false);
+    }
+        window.addEventListener('scroll',handleScroll)
+        return ()=>{
+            window.removeEventListener("scroll",handleScroll);
+        };
+    },[])
+
   return (
-    <div></div>
+    <div className={`Nav ${show && 'Nav_black'}`}>
+        <img 
+        className='Netflix-logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
+        alt='NETFLIX' 
+        />
+        <img 
+        className='user-logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+        alt='AVATAR' 
+        />
+    </div>
   )
 }
 
